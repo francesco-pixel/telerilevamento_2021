50d3d85fc7dd0edd4b193c73e4294dd1f4a8493f
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-03-28 00:34:17 +0100

    Update R_code_remote_sensing_first_.r

--- R_code_remote_sensing_first_.r
+++ R_code_remote_sensing_first_.r
@@ -9,7 +9,7 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 # Attraverso la funzione brick ho importato l'intera immagine! 
 
 p224r63_2011 
-# successivamento digitando il nome dell'immagine ho visualizzato tutte le sue informazioni (tipo di file, risoluzione etc)
+# successivamento digitando il nome dell'immagine ho visualizzato tutte le sue informazioni (tipo di file, risoluzione, informazioni sui sistemi di riferimento etc)
 
 plot(p224r63_2011)
 # ho fatto il primo plot con sette bande con dei dati satellitari!!
