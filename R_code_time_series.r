6171b5d6038fad2e832f1c04359ecc611d72b74f
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-16 12:55:14 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,10 @@
+#Time series analysis
+#Variazione delle temperature in Groenlandia nel tempo ----> Time series analysis
+# Dati di Emanuela Cosma 
+# Primo pacchetto raster
+
+setwd("C:/lab/greenland") # Windows
+#utilizziamo la working directory greenland
+# install.packages("raster")
+
+library(raster)
