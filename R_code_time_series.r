2e46105530879d949701cd51699ee122c6ecd5b2
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-17 00:07:06 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -11,4 +11,4 @@ library(raster)
 #stack: insieme di dati multitemporali, in questo caso raster.
 #importiamo i 4 strati separati che rappresentano la stima della temperatura che deriva dal programma Copernicus, consieriamo i primi 10 giorni di giugno nel 2000 2005 2010 2015
 #la funzione per caricare singoli dati non si chiama piu brick ma raster. All'interno del pacchetto raster c'è una funzione che si chiama raster:
-lst_2000 <- raster("lst_2000.tif")
+lst_2000 <- raster("lst_2000.tif")jj
