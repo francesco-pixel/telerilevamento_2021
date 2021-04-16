f4775360f5c72350f81bd69d125651a7ff961990
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-16 15:57:04 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -4,7 +4,11 @@
 # Primo pacchetto raster
 
 setwd("C:/lab/greenland") # Windows
+library(raster)
 #utilizziamo la working directory greenland
 # install.packages("raster")
 
-library(raster)
+#stack: insieme di dati multitemporali, in questo caso raster.
+#importiamo i 4 strati separati che rappresentano la stima della temperatura che deriva dal programma Copernicus, consieriamo i primi 10 giorni di giugno nel 2000 2005 2010 2015
+#la funzione per caricare singoli dati non si chiama piu brick ma raster. All'interno del pacchetto raster c'è una funzione che si chiama raster:
+lst_2000 <- raster("lst_2000.tif")
