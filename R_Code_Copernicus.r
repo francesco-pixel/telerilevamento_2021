916e8528e7399ed781f31faf99a4f261fec09e8f
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-24 18:21:27 +0200

    Update R_Code_Copernicus.r

--- R_Code_Copernicus.r
+++ R_Code_Copernicus.r
@@ -8,4 +8,17 @@ install.packages("ncdf4")
 library("ncdf4")
 #libreria per leggere netCDF
 
-setwd()
+setwd("C:/lab/") 
+
+#Diamo un nome al nostro dataset, lo carichiamo  usando la funzione raster per caricare  un singolo strato, mettiamo tra virgolette il nome del file copiato e incollato includendo l'estensione .nc
+albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
+albedo #vediamo tutte le info del dataset
+
+#adesso possiamo plottare la prima immagine, non montiamo in RGB ma decidiamo noi la scala di colori da utilizzare.
+
+plot(test)
+
+ext <- c(6, 20, 35, 50)
+testc <- crop(test, ext)
+plot(testc)
+
