913b6095c36a63d88ebc35f805c2b1d69d1354a2
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-24 19:05:35 +0200

    Update R_Code_Copernicus.r

--- R_Code_Copernicus.r
+++ R_Code_Copernicus.r
@@ -15,8 +15,30 @@ albedo <- raster("c_gls_ALBH_202006130000_GLOBE_PROBAV_V1.5.1.nc")
 albedo #vediamo tutte le info del dataset
 
 #adesso possiamo plottare la prima immagine, non montiamo in RGB ma decidiamo noi la scala di colori da utilizzare.
+cl <- colorRampPalette(c('black','grey','yellow','red'))(100)
 
-plot(test)
+
+plot(albedo,col=cl)
+#dunque vediamo la quantità e la superficie doce viene riflessa più energia solare (la parte del deserto riflette maggiormente)
+
+#ggplot2 ci permette di abbellire i grafici
+
+#ora applichiamo la funzione aggregate per aggregare i pixel
+albedores <- aggregate(albedo, fact=50)
+#prendo pixel piu grandi, trasformando l'immagine con un minor numero di pixel per un determinato fattore
+albedores
+
+plot(albedores)
+#noto come la variabile sia più veloce da visualizzare
+
+#Data la variabile molto pesante, adesso la ricampioniamo  per un fattore 100 diminuendo la dimensione di 1000 volte e rifaccio il plot di albedores:
+#ricampionamento bilineare:
+
+albedores <- aggregate(albedo, fact=100)
+plot(albedores, col=cl)
+
+
+
 
 ext <- c(6, 20, 35, 50)
 testc <- crop(test, ext)
