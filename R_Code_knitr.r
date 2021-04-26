170f78555c9a4a8722dc3d55cb00db26be2e7aab
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-26 11:10:44 +0200

    Update R_Code_knitr.r

--- R_Code_knitr.r
+++ R_Code_knitr.r
@@ -1,1 +1,7 @@
+#day 16/04
+generiamo il codice che utilizzeremo dentro knitr 
 
+
+
+require(knitr)
+stitch("~/Downloads/R_code_temp.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
