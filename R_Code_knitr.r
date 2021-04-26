c5a95ea5bef6788f3e9291ef8f06c54771ed6a64
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-26 11:26:53 +0200

    Update R_Code_knitr.r

--- R_Code_knitr.r
+++ R_Code_knitr.r
@@ -1,5 +1,15 @@
 #day 16/04
+
+setwd("C:/lab/") 
+
+
 generiamo il codice che utilizzeremo dentro knitr 
+# all'interno di R abbiamo il pacchetto knitr. e questo pacchetto dentro il nostro softwere puo utilizzare un codice esterno, quindi knotr va a prendere il codice all'esterno,lo importa dentro R e all'interno di R genera un report che verra salvato nella stessa cartella dove è presente il codice precedente.
+#installiamo knitr
+install.packages("knitr")
+library("knitr")
+
+
 
 
 
