2fbc0038c59cd2072fc3cd9269cbe02f9316caa3
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-28 15:32:24 +0200

    Create R_Code_multucariate_analisys.r

---
+++ R_Code_multucariate_analisys.r
@@ -0,0 +1,5 @@
+#sensore atex dove ci sono centinaia di bande e di  icrosensori, possiamo compattare il dataset tramite analisi multivariata e vederlo in due dimensioni.
+
+#Concetto di variabilità di un sistema 
+
+#Asse componente principale 
