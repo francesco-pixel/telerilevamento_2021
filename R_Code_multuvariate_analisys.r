9ac3b795f6b475615ae8b8c3e27df34e98e0b05b
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-04-29 18:59:24 +0200

    Update R_Code_multuvariate_analisys.r

--- R_Code_multuvariate_analisys.r
+++ R_Code_multuvariate_analisys.r
@@ -43,12 +43,41 @@ pairs(p224r63_2011)
 p224r63_2011res <- aggregate(p224r63_2011, fact=10) #aggreghiamo di un fattore 10, diminuendo la risolzione da 30m a 300m aumentando la grandezza del pixel diminuiamo la risoluzione
 
 #adesso per vederlo facciamo un pannello con 2 immagini e lo plottiamo
-par(mfrow=c(2,1)
+par(mfrow=c(2,1))
     
     
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin") 
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin") #dato originale con tanti pixel
 plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="lin") 
 #quella sopra 30x30
 #quella sotto pixel 300x300m #quindi quella ricampionata
+
+#raster PCA: prende il pacchetto dei dati e li compatta in un numero minore di bande
+p224r63_2011res_pca <- rasterPCA(p224r63_2011res)
+
+#quindi abbiamo preso l'immagine originale abbiamo fatto la PCA e generato una nuova immagine , creato una mappa in uscita e un modello 
+#la funzione summary ci da un sommario del nostro modello e visualizzarlo
+
+summary(p224r63_2011res_pca$model) 
+
+#facciamo il plot totale
+
+
+plot(p224r63_2011res_pca$map) 
+
+p224r63_2011res_pca
+
+
+#la prima componente con molta informazione mentre l'ultima la 7 non ha molta info. la prima presenta molta variabilità
+
+
+#plottiamo in rgb tutta l'immagine con le 3 componenti principali
+
+plotRGB(p224r63_2011res_pca$map, r=4, g=3, b=2, stretch="lin") 
+#immagine risultante dall'analisi delle 3 componenti principali
+
+
+
+
+
         
  
