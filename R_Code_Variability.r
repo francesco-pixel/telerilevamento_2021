60f52425dbd6546f2a3b94c62edc043ae8ffbbb0
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-07-20 18:14:08 +0200

    Update R_Code_Variability.r

--- R_Code_Variability.r
+++ R_Code_Variability.r
@@ -1,7 +1,11 @@
 #R_Code_Variability.r
 library(raster)
 library(RStoolbox)
-# install.packages("RStoolbox")
+library(ggplot2) # for ggplot plotting
+library(gridExtra) # for plotting ggplots together
+# install.packages("viridis")
+install.packages("viridis") # for ggplot colouringll.packages("RStoolbox")
+
 setwd("C:/lab/") # Windows
 #l'immagine sentinel è composta da tre strati near infrared il red al secondo e green al terzo livello
 #il raster si utilizza solo per catturare il primo strato quindi usiamo brick cosi portiamo dentro l'intero blocco
@@ -63,6 +67,62 @@ sentpca <- rasterPCA(sent)
 plot(sentpca$map)  
 #osserviamo le 4 componenti che ha, via via che si passa da una pca all'altra si perdono informazioni. i layer sono 4 pc1-2-3-4.
 #vediamo quanta variabilita spiegano le singole componenti #summary:
-#la prima componente principale spiega il 67,36 % della proporzione principale
+#la prima componente principale spiega il 67,36 % della proporzione principale e lo abbiamo visto attraverso summary
 summary(sentpca$model)
  #la prima PC spiega il 67,36804 % dell'informazione totale e originale 
+#la nostra immagine sentpca la leghiamo a map con il dollero ed essa cconterra 4 variabili, noi prenderemoo la PC questo lo associamo ad un ogetto cche si chiama PC1 e aplichiama la funzione focal a PC1
+
+#e useremo questo oggetto PC1 per applicare focal
+pc1 <- sentpca$map$PC1
+#facciamo la dev standard tramite focal sulla PC1 usando direttamente una griglia 5x5 e cambiamo la colorampalette, poi plottiamo
+pc1sd5 <- focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(pc1sd5, col=clsd)
+#la confrontiamo con l'immagine sent del similaun originale del ghiacciaio
+#usando la pc1 vediamo la parte di vegetazione è quella piu omogenea mentre un aumento di variabilità nelle zone di roccia (metodo ideale per vedere la variabilità
+#la funzione source ci permette di richiamare un pezzo di codice che abbiamo gia creato in precedenza. lo facciamo con una mappa della standard dev di 7x7 e lo salvo nella cartella lab per poi andarla a richiamare tramite source dalla cartella lab.
+# pc1 <- sentpca$map$PC1
+# pc1sd7 <- focal(pc1, w=matrix(1/49, nrow=7, ncol=7), fun=sd)
+# plot(pc1sd7)
+#adesso la richiamo tramite source e lo plotto 
+source("source_test_lezione.r")
+#questa è la dev standard di una finestra 7x7 pixel
+#altro esempio per rrochiamare uno script
+source("source_ggplot.r.txt")
+#ggplot permette di fare dei plottaggi piu belli
+#creiamo una nuova finestra vuota e aggiungiamo dei blocchi, ad esempio una geometria geom_point crea dei punti ad esempio.
+ggplot()
+
+#useremo la funzione geom_raster della mappa creata della pca
+
+p1 <- ggplot() + geom_raster(pc1_devst, mapping = aes(x = x, y = y, fill = layer)) + scale_fill_viridis() + ggtitle("Standard deviation of PC1by viridis colour scale")
+#ho definito quale raster usare e le componenti mapping
+#definito le estetiche e cioe cosa plottimo x e y e tutti i valori interni e cioe il layer. la funzione per estetics dentro geom_raster è mapping e cioe cosa vogliamo mappare.
+#abbiamo plottato con ggplot e vediamo il valore della dev.standard, vediamo tutte le discontinuità ed il dato è molto omogeneo, si osservano bene le varibilità ecologiche e geomorfologiche von ggplot.
+#metodo migliore per individuare tutte le variabilità, anche geologiche.
+#ed ho aggiunto l'utilizzo delle legende viridis di default ed aggiunto il titolo
+
+#provo ad usare un'altra legeda "magma" e facciamo un altro ggplot
+p2 <- ggplot() + geom_raster(pc1_devst, mapping = aes(x = x, y = y, fill = layer)) + scale_fill_viridis(option = "magma") + ggtitle("Standard deviation of PC1by magma colour scale")
+
+#vediamo molto bene le parti con dev.standard elevata
+#usiamo legenda "inferno"
+p3 <- ggplot() + geom_raster(pc1_devst, mapping = aes(x = x, y = y, fill = layer)) + scale_fill_viridis(option = "inferno") + ggtitle("Standard deviation of PC1by inferno colour scale")
+#legenda turbo
+p4 <- ggplot() + geom_raster(pc1_devst, mapping = aes(x = x, y = y, fill = layer)) + scale_fill_viridis(option = "turbo") + ggtitle("Standard deviation of PC1by turbo colour scale")
+
+#proviamo a mettere le tre mappe create con le diverse legende insieme, abbiamo le tre mappe viridis, magma e turbo, che associamo a p1,p2, p3 e p4 le mettiamo insieme tramite grid.arrange dalla libreria grid extra
+grid.arrange(p1, p2, p3, p4, nrow = 1) 
+#viridis e magma sono le piu utili, l'occhio umano èpiu colpito dal colore giallo
+
+
+
+
+
+
+
+
+
+
+
+
