c857f1e4d2a30adf9e7ca0af2321765b4a31cd40
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-07-28 17:48:14 +0200

    Update R_code_nO2.r

--- R_code_nO2.r
+++ R_code_nO2.r
@@ -6,7 +6,7 @@ library(raster)
 #Set the work directory EN
 
 
-setwd("C:/lab/EN") windows
+setwd("C:/lab/EN") #windows
 
 #importo la prima immagine e quindi la prima banda 
 
@@ -14,12 +14,12 @@ EN01 <- raster("EN_0001.png") #gennaio
 
 #plotto la prima immagine con la mia color ramp palette preferita
 cls <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(200) # 
-plot(EN_0001.png, col=cls)
+plot(EN01, col=cls)
 
 #importo la tredicesima immagine e la plotto con l'ultima color ramp palette precedente
-EN013 <- raster("EN_0013.png") #marzo
-cls <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(200) # 
-plot(EN_0013.png, col=cls)
+EN13 <- raster("EN_0013.png") #marzo
+cls <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(200)  
+plot(EN13, col=cls)
 
 #faccio la differebza fra le due immagini, lo associo ad un oggetto e poi la plotto, marzo meno gennaio ma posso fare anche gennaio meno marzo.
 
@@ -28,6 +28,6 @@ plot (ENdif, col=cls)
 
 #plotto tutte e tre le immagini ricavate insieme
 par(mfrow=c(3,1))
-plot(EN_0001.png, col=cls main= "NO2 a gennaio)
-plot(EN_0013.png, col=cls main= "NO2 a marzo)
-plot (ENdif, col=cls main= "NO2 difference)
+plot(EN01, col=cls, main="NO2 difference gennaio")
+plot(EN13, col=cls, main="NO2 difference marzo")
+plot(ENdif, col=cls, main="NO2 difference differenze")
