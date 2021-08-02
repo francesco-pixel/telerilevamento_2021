e406b491c7d61a2202928939638141eff77cea13
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-08-02 23:30:12 +0200

    Update R_code_project_esame.r

--- R_code_project_esame.r
+++ R_code_project_esame.r
@@ -1,2 +1,16 @@
 R_code_project_exam.r
 
+#R_code variazione della land cover nella parte superiore del bacino idrografico del Fiume Missisipi 
+#Durante la prima metà del 1993, il Midwest statunitense ha subito piogge insolitamente forti. Gran parte degli Stati Uniti nella parte superiore del bacino
+#idrografico del fiume Mississippi ricevettero più di 1,5 volte le loro precipitazioni medie nei primi sei mesi dell'anno.#
+#Questa coppia di immagini mostra l'area intorno a St. Louis, Missouri, nell'agosto 1991 e nel 1993. 
+#L'immagine del 1993 è stata catturata leggermente dopo il picco dei livelli dell'acqua in questa parte del fiume Mississippi.
+#Questa immagine a falsi colori è stata creata combinando lunghezze d'onda infrarosse,
+#infrarosse vicine all'infrarosso e verdi di luce osservate dallo strumento Thematic Mapper (TM) a bordo del satellite Landsat
+#L'acqua appare blu scuro, la vegetazione sana è verde, i campi nudi e il terreno appena esposto sono rosa e il cemento è grigio. 
+#Inoltre, il diffuso cambiamento della copertura del suolo lungo fiumi e torrenti ha modificato drasticamente i sistemi naturali di controllo
+#Sensori:
+#Landsat 5 - TM
+
+# 14 agosto 1991
+# 19 agosto 1993
