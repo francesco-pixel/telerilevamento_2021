17ce1ca6ddff0c81da10a8a5fd691becffad7d74
francesco-pixel <81394134+francesco-pixel@users.noreply.github.com>
2021-08-03 00:18:24 +0200

    Update R_code_project_esame.r

--- R_code_project_esame.r
+++ R_code_project_esame.r
@@ -1,4 +1,6 @@
-R_code_project_exam.r
+#R_code_project_exam.r
+#Analisi della land cover del Fiume del Missisipi
+
 
 #R_code variazione della land cover nella parte superiore del bacino idrografico del Fiume Missisipi 
 #Durante la prima metà del 1993, il Midwest statunitense ha subito piogge insolitamente forti. Gran parte degli Stati Uniti nella parte superiore del bacino
@@ -14,3 +16,85 @@ R_code_project_exam.r
 
 # 14 agosto 1991
 # 19 agosto 1993
+
+
+##Utilizzo gli algoritmi di classificazione delle immagini e faccio un analisi multitemporale della variazione della copertura del suolo, successivamente calcolo alcuni parametri.
+
+
+setwd("C:/lab/")
+
+
+library(raster)
+
+
+install.packages("RStoolbox") 
+library(RStoolbox)#per la classificazione dell immagini
+
+install.packages("ggplot2")
+library(ggplot2) #per effettuare dei plottaggi dettagliati
+#installo anche gridExtra per effettuare piu plottaggi insieme c
+
+install.packages("gridExtra")
+library(gridExtra)
+
+
+
+#carico la prima immagine con la funzione brick
+stlouis91 <- brick("stlouis91.jpg") 
+
+
+
+plotRGB(stlouis91, r=1, g=2, b=3, stretch="Lin")
+#all'interno di ggplot ci sono funzioni potenti per plottare immagini----> funzioni con gg
+#funzione ggRGB, essa ha bisogno dell' immagine , delle componenti RGB e stretch.
+ggRGB(stlouis91, r=1, g=2, b=3, stretch="Lin") #otteniamo un plot con le coordinate spaziali (plot migliore)
+
+
+#carico adesso la seconda immagine ed effettuo le stesse operazioni.
+
+stlouis93 <- brick("stlouis93.jpg")
+plotRGB(stlouis93, r=1, g=2, b=3, stretch="Lin")
+ggRGB(stlouis93, r=1, g=2, b=3, stretch="Lin")
+
+#metto le immagine plotRGB accanto con la funzione parmfrow e le metto a confronto
+par(mfrow=c(1,2))
+plotRGB(stlouis91, r=1, g=2, b=3, stretch="Lin")
+plotRGB(stlouis93, r=1, g=2, b=3, stretch="Lin")
+
+
+
+
+
+
+
+
+#usiamo adesso la funzione grid.arrange che mette insieme vari pezzi dentro il grafico
+p1 <- ggRGB(stlouis91, r=1, g=2, b=3, stretch="Lin")
+p2 <- ggRGB(stlouis93, r=1, g=2, b=3, stretch="Lin")
+grid.arrange(p1, p2, nrow=2)
+#immagini disposte su due righe
+
+
+
+
+#facciamo l'unsupervised(non viene supervisionata da noi inizialmente) classification 
+# classificazione non supervisionata
+d1c <- unsuperClass(stlouis91, nClasses=2) #immmagine e numero di classi
+d1c #due valori
+#abbiamo d1c che è il modello e la mappa che abbiamo creato e lo plotiamo
+plot(d1c$map)
+#classe 1 agricola
+#classe due non agricola
+
+
+
+
+
+
+
+
+
+
+
+
+
